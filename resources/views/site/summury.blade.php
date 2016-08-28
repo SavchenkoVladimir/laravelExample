8f6b97c35411ed5918f3d838eb77748f27c402bf
daf <daf@localhost>
2016-08-28 11:46:27 +0300

    Summary page is done.

--- resources/views/site/summury.blade.php
+++ resources/views/site/summury.blade.php
@@ -1,40 +1,40 @@
 @extends('site.layout')
 
 @section('content')
-<div id="summury">
-    <div class="row sum">
-        <div class="hidden-xs col-sm-1 col-md-1 col-lg-1 sum"></div>
-        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-2 sum">
+<div id="summary">
+    <div class="sum row">
+        <div class="sum hidden-xs hidden-sm col-md-1 col-lg-1"></div>
+        <div class="sum col-xs-12 col-sm-4 col-md-3 col-lg-3">
             <div class="row">
-                <div class="col-xs-4 col-sm-12 col-md-12 col-lg-12 sum">
-                    <img class="face sum" src="{!! asset('asset/img/site/030.jpg') !!}" alt="author photo" />
+                <div class="sum col-xs-5 col-sm-12 col-md-12 col-lg-12">
+                    <img class="sum face" src="{!! asset('asset/img/site/030.jpg') !!}" alt="author photo" />
+                    <hr class="sum face">
                 </div>
-                <hr class="face">
-                <div class="col-xs-5 col-sm-12 col-md-12 col-lg-12 sum">
-                    <div class="contacts">
-                        <div class="skype">
+                <div class="sum col-xs-7 col-sm-12 col-md-12 col-lg-12">
+                    <div class="sum contacts">
+                        <div class="sum skype">
                             <img src="{!! asset('asset/img/site/skype.jpg') !!}" alt="type message by skype image" class="sum" />
-                            <p><a href="skype:SavchenkoV.V.?chat">SavchenkoV.V.</a></p>					
+                            <p class="sum"><a href="skype:SavchenkoV.V.?chat">SavchenkoV.V.</a></p>					
                         </div>
-                        <div class="email">
+                        <div class="sum email">
                             <img src="{!! asset('asset/img/site/mail.png') !!}" alt="type message by email image" class="sum" />
-                            <p><a href="mailto:Peers.Rom@gmail.com">Peers.Rom@gmail.com</a></p>					
+                            <p class="sum"><a href="mailto:Peers.Rom@gmail.com">Peers.Rom@gmail.com</a></p>					
                         </div>
-                        <div class="tel">
+                        <div class="sum tel">
                             <img src="{!! asset('asset/img/site/phone.png') !!}" alt="telephone logo" class="sum" />
-                            <p>+38(050)759-20-16</p>					
+                            <p class="sum">+38(050)759-20-16</p>					
                         </div>				
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-8 sum">
-            <div class="download">
+        <div class="sum col-xs-12 col-sm-8 col-md-7 col-lg-7">
+            <div class="sum download">
                 <a download href="{!! asset('asset/txt/#') !!}Vladimir_Savchenko_Junior_Trainee_QA_Engineer_CV.pdf">
                     download PDF CV</a>
-                <img src="{!! asset('asset/img/site/downloadPDF.png') !!}" alt="download image" />
+                <img src="{!! asset('asset/img/site/downloadPDF.png') !!}" class="sum download" alt="download image" />
             </div>
-            <div class="about">
+            <div class="sum about">
                 <h3>Hello, friends.</h3>
                 <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p><br />
                 <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p><br />
@@ -42,23 +42,23 @@
                 <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p><br />
                 <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p><br />
                 <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p><br />
-
-                <div class="rule">
-                    <h4>Rule</h4>
-                    <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>
+                <div class="sum rule-group">
+                    <div class="sum rule">
+                        <h4>Rule</h4>
+                        <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>
+                    </div>
+                    <div class="sum rule">
+                        <h4>Rule</h4>
+                        <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>				
+                    </div>
+                    <div class="sum rule">				
+                        <h4>Rule</h4>
+                        <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>
+                    </div>
                 </div>
-                <div class="rule">
-                    <h4>Rule</h4>
-                    <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>				
-                </div>
-                <div class="rule">				
-                    <h4>Rule</h4>
-                    <p>Различного рода тени (особенно необычной формы), как правило делаются с помощью функций Photoshop Blending. Но сейчас, когда у нас есть CSS3, можно делать и кое-что поинтереснее, чем просто тени, не прибегая к помощи инструментариев от Adobe.</p>
-                </div>
-
             </div>
         </div>
-        <div class="hidden-xs col-sm-1 col-md-4 col-lg-1 sum"></div>
+        <div class="sum hidden-xs hidden-sm col-md-4 col-lg-1"></div>
     </div>
 </div>
 @endsection
